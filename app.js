8514e5d74ae3dc7d90766093d20571ccc912624f
James Pask <james@jpask.com>
2024-04-16 11:43:46 +0100

    Implement Game Scoring Function

--- app.js
+++ app.js
@@ -10,29 +10,57 @@ function getComputerChoice() {
 function getPlayerChoice() {
   let choice = window.prompt('Enter your choice');
   choice = choice.toLowerCase();
-  console.log(choice);
+  // console.log(choice);
   return choice;
 }
 
+let comScore = 0;
+let playerScore = 0;
 // Game run Function
 function playRound(playerSelection, computerSelection) {
   // console.log(`Player Selection: ${playerSelection}`);
   // console.log(`COM Selection: ${computerSelection}`);
   if (playerSelection === computerSelection) {
+    comScore += 1;
+    playerScore += 1;
     return `It's a draw!`;
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
     // console.log('rock/scissor');
-    return `Player Wins`;
+    playerScore += 1;
+    return `Player Wins, Rock beats Scissors`;
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
     // console.log('scissor/paper');
-    return `Player Wins`;
+    playerScore += 1;
+    return `Player Wins, Scissors beats Paper`;
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
     // console.log('paper/rock');
-    return `Player Wins`;
-  } else {
-    return `Computer Wins`;
+    playerScore += 1;
+    return `Player Wins, Paper beats Rock`;
+  } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
+    comScore += 1;
+    return `Computer Wins, Rock beats Scissors`;
+  } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+    comScore += 1;
+    return `Computer Wins, Scissors beats Paper`;
+  } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+    comScore += 1;
+    return `Computer Wins, Paper beats Rock`;
   }
 }
 
-console.log(playRound(getPlayerChoice(), getComputerChoice()));
-//
+// This calls the function which gives a new random which was causing confusion
+// console.log(playRound(getPlayerChoice(), getComputerChoice()));
+
+for (let i = 0; i < 5; i++) {
+  console.log(playRound(getPlayerChoice(), getComputerChoice()));
+}
+if (playerScore > comScore) {
+  console.log('Player Wins');
+  console.log(`Final Score: Player: ${playerScore} / COM: ${comScore}`);
+} else if (playerScore < comScore) {
+  console.log('COM Wins');
+  console.log(`Final Score: Player: ${playerScore} / COM: ${comScore}`);
+} else {
+  console.log(`It's a draw`);
+  console.log(`Final Score: Player: ${playerScore} / COM: ${comScore}`);
+}
