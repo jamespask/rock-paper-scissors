e2a3601db5d88687b0cfa2e44b7654a52ed9e091
James Pask <james@jpask.com>
2024-04-16 11:17:12 +0100

    Implement Player Choice Function

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 // Randomly Generate COM choice
 function getComputerChoice() {
-  let choices = ['Rock', 'Paper', 'Scissors'];
+  let choices = ['rock', 'paper', 'scissors'];
   // Random Number from Array length to use as index
   let rand = Math.floor(Math.random() * choices.length);
   return choices[rand];
 }
 
-console.log(getComputerChoice());
+// Get player Choice
+function getPlayerChoice() {
+  let choice = window.prompt('Enter your choice');
+  choice = choice.toLowerCase();
+  console.log(choice);
+  return choice;
+}
 
 // Game run Function
-// Case insensitive input
-// Params (COM Choice, Player Choice)
-//
+function playRound(playerSelection, computerSelection) {
+  // console.log(`Player Selection: ${playerSelection}`);
+  // console.log(`COM Selection: ${computerSelection}`);
+  if (playerSelection === computerSelection) {
+    return `It's a draw!`;
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    // console.log('rock/scissor');
+    return `Player Wins`;
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    // console.log('scissor/paper');
+    return `Player Wins`;
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    // console.log('paper/rock');
+    return `Player Wins`;
+  } else {
+    return `Computer Wins`;
+  }
+}
+
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
